a395184cdd68120d0a579a9e59d6bfb5e844d618
Maria290301 <mezhelevskayamaria@yandex.ru>
2025-06-28 18:16:39 +0300

    Дипломный проект. Проверка порта 9090

--- stats-server/server/src/main/java/ru/practicum/EwmStatsCoreApplication.java
+++ stats-server/server/src/main/java/ru/practicum/EwmStatsCoreApplication.java
@@ -1,11 +1,15 @@
 package ru.practicum;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EwmStatsCoreApplication {
     public static void main(String[] args) {
+        log.info("Starting stats server...");
         SpringApplication.run(EwmStatsCoreApplication.class, args);
+        log.info("Stats server started successfully.");
     }
 }
